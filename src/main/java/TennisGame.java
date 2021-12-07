bb3cad84c9f7182f9a9368d3167ed896b52c84b6
Alisa-InWonderland <alisa_ne@yahoo.com>
2021-12-07 15:45:33 +0100

    Initial commit

---
+++ src/main/java/TennisGame.java
@@ -0,0 +1,83 @@
+public class TennisGame {
+
+    int player1Score = 0;
+    int player2Score = 0;
+
+
+    public TennisGame() {
+    }
+
+    public void wonPoint(String player) {
+
+        if (player == "player1") {
+            player1Score += 1;
+        } else if (player == "player2") {
+            player2Score += 1;
+        }
+    };
+
+    public String getScore() {
+
+        String puntuacionPlayer1 = representation(player1Score);
+        String puntuacionPlayer2 = representation(player2Score);
+
+        if (player1Score == 3 && player2Score == 3) {
+            return "Deuce";
+        }
+        else if (player1Score == 4 && player2Score == 3) {
+            return "Advantage player1";
+        }
+
+        else if (player1Score == 3 && player2Score == 4) {
+            return "Advantage player2";
+        }
+
+        else if (player1Score == 5 && player2Score == 3) {
+            return "Win for player1";
+        }
+
+        else if (player1Score == 3 && player2Score == 5) {
+            return "Win for player2";
+        }
+        return puntuacionPlayer1 + " - " + puntuacionPlayer2;
+    }
+
+    private String representation(int score) {
+        switch (score){
+            case 0:
+                return "love";
+            case 1:
+                return "fifteen";
+            case 2:
+                return "thirty";
+            default:
+                return "forty";
+        }
+    }
+}
+
+
+
+
+
+
+/**switch(player1Score, player2Score) {
+        case 5:
+        System.out.println("Habitación 1");
+        break;
+        case 10:
+        System.out.println("Habitación 2");
+        break;
+        case 15:
+        System.out.println("Habitación 3");
+        break;
+        case 20:
+        System.out.println("Habitación 4");
+        break;
+default:
+        System.out.println("Habitación 5");
+        }
+
+        System.out.println("Fuera del switch (y de las habitaciones)");
+        }
+ **/
---
+++ src/test/java/TennisTest.java
@@ -0,0 +1,154 @@
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class TennisTest {
+    @Test
+    void cuandoElJuegoEmpiezaElMarcadorEstaEn0() {
+        TennisGame juego = new TennisGame();
+        String resultado = juego.getScore();
+        assertThat(resultado, equalTo("love - love"));
+    }
+
+    @Test
+    void cuandoElJugador1MarcaUnPunto() {
+        TennisGame juego = new TennisGame();
+        juego.wonPoint("player1");
+        String resultado = juego.getScore();
+        assertThat(resultado, equalTo("fifteen - love"));
+    }
+
+    @Test
+    void cuandoLosDosJugadoresMarcanUnPunto() {
+        TennisGame juego = new TennisGame();
+        juego.wonPoint("player1");
+        juego.wonPoint("player2");
+        String resultado = juego.getScore();
+        assertThat(resultado, equalTo("fifteen - fifteen"));
+    }
+    @Test
+    void cuandoElJugador1Marca2PuntosYElJugador2Marca1Punto() {
+        TennisGame juego = new TennisGame();
+        juego.wonPoint("player1");
+        juego.wonPoint("player1");
+        juego.wonPoint("player2");
+        String resultado = juego.getScore();
+        assertThat(resultado, equalTo("thirty - fifteen"));
+    }
+
+    @Test
+    void cuandoElJugador1Marca1PuntoYElJugador2Marca2Puntos() {
+        TennisGame juego = new TennisGame();
+        juego.wonPoint("player1");
+        juego.wonPoint("player2");
+        juego.wonPoint("player2");
+        String resultado = juego.getScore();
+        assertThat(resultado, equalTo("fifteen - thirty"));
+    }
+
+    @Test
+    void cuandoLosDosJugadoresMarcan2Puntos() {
+        TennisGame juego = new TennisGame();
+        juego.wonPoint("player1");
+        juego.wonPoint("player1");
+        juego.wonPoint("player2");
+        juego.wonPoint("player2");
+        String resultado = juego.getScore();
+        assertThat(resultado, equalTo("thirty - thirty"));
+    }
+
+    @Test
+    void cuandoElJugador1Marca3PuntosYElJugador2Marca2Puntos() {
+        TennisGame juego = new TennisGame();
+        juego.wonPoint("player1");
+        juego.wonPoint("player1");
+        juego.wonPoint("player1");
+        juego.wonPoint("player2");
+        juego.wonPoint("player2");
+        String resultado = juego.getScore();
+        assertThat(resultado, equalTo("forty - thirty"));
+    }
+
+    @Test
+    void cuandoElJugador1Marca2PuntosYElJugador2Marca3Puntos() {
+        TennisGame juego = new TennisGame();
+        juego.wonPoint("player1");
+        juego.wonPoint("player1");
+        juego.wonPoint("player2");
+        juego.wonPoint("player2");
+        juego.wonPoint("player2");
+        String resultado = juego.getScore();
+        assertThat(resultado, equalTo("thirty - forty"));
+    }
+
+    @Test
+    void cuandoLosDosJugadoresMarcan3Puntos() {
+        TennisGame juego = new TennisGame();
+        juego.wonPoint("player1");
+        juego.wonPoint("player1");
+        juego.wonPoint("player1");
+        juego.wonPoint("player2");
+        juego.wonPoint("player2");
+        juego.wonPoint("player2");
+        String resultado = juego.getScore();
+        assertThat(resultado, equalTo("Deuce"));
+    }
+    @Test
+    void cuandoElJugador1Marca4PuntosYElJugador2Marca3Puntos() {
+        TennisGame juego = new TennisGame();
+        juego.wonPoint("player1");
+        juego.wonPoint("player1");
+        juego.wonPoint("player1");
+        juego.wonPoint("player1");
+        juego.wonPoint("player2");
+        juego.wonPoint("player2");
+        juego.wonPoint("player2");
+        String resultado = juego.getScore();
+        assertThat(resultado, equalTo("Advantage player1"));
+    }
+
+    @Test
+    void cuandoElJugador1Marca3PuntosYElJugador2Marca4Puntos() {
+        TennisGame juego = new TennisGame();
+        juego.wonPoint("player1");
+        juego.wonPoint("player1");
+        juego.wonPoint("player1");
+        juego.wonPoint("player2");
+        juego.wonPoint("player2");
+        juego.wonPoint("player2");
+        juego.wonPoint("player2");
+        String resultado = juego.getScore();
+        assertThat(resultado, equalTo("Advantage player2"));
+    }
+
+    @Test
+    void cuandoElJugador1Marca5PuntosYElJugador2Marca3Puntos() {
+        TennisGame juego = new TennisGame();
+        juego.wonPoint("player1");
+        juego.wonPoint("player1");
+        juego.wonPoint("player1");
+        juego.wonPoint("player1");
+        juego.wonPoint("player1");
+        juego.wonPoint("player2");
+        juego.wonPoint("player2");
+        juego.wonPoint("player2");
+        String resultado = juego.getScore();
+        assertThat(resultado, equalTo("Win for player1"));
+    }
+
+    @Test
+    void cuandoElJugador1Marca3PuntosYElJugador2Marca5Puntos() {
+        TennisGame juego = new TennisGame();
+        juego.wonPoint("player1");
+        juego.wonPoint("player1");
+        juego.wonPoint("player1");
+        juego.wonPoint("player2");
+        juego.wonPoint("player2");
+        juego.wonPoint("player2");
+        juego.wonPoint("player2");
+        juego.wonPoint("player2");
+        String resultado = juego.getScore();
+        assertThat(resultado, equalTo("Win for player2"));
+    }
+}
